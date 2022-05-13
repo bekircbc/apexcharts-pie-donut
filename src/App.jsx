508fdf956906bce999e783ead49c698a83cb34c1
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-13 15:15:05 +0200

    code is still not working. but one chart can be seen.

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,57 @@ import { useState, useEffect } from "react";
 export const App = () => {
   const [options, setOptions] = useState({});
   const [series, setSeries] = useState([]);
-  const [type, setType] = useState("pie");
+  const [type, setType] = useState("bar");
+
+  const lineOptions = {
+    chart: {
+      id: "apexchart-example",
+    },
+    xaxis: {
+      categories: [
+        2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022,
+      ],
+    },
+  };
+
+  const lineSeries = [
+    {
+      name: "series-1",
+      data: [30, 40, 35, 50, 49, 60, 70, 91, 125, 30, 45, 65],
+    },
+    {
+      name: "series-2",
+      data: [20, 30, 55, 75, 63, 60, 80, 134, 110, 40, 55, 64],
+    },
+  ];
+
+  const pieOptions = {
+    labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+    legend: { position: "bottom" },
+  };
+
+  const pieSeries = [
+    {
+      name: "series",
+      data: [44, 55, 13, 43, 22],
+    },
+  ];
 
   useEffect(() => {
-    setOptions({
-      labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
-      legend: { position: "bottom" },
-      // xaxis: {
-      //   categories: [
-      //     2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
-      //     2022,
-      //   ],
-    });
-    setSeries([
-      {
-        name: "series",
-        data: [44, 55, 13, 43, 22],
-      },
-    ]);
-
-    //   [
-    //   {
-    //     name: "series-1",
-    //     data: [30, 40, 35, 50, 49, 60, 70, 91, 125, 30, 45, 65],
-    //   },
-    //   {
-    //     name: "series-2",
-    //     data: [20, 30, 55, 75, 63, 60, 80, 134, 110, 40, 55, 64],
-    //   },
-    // ]
+    if (type === ("line" || "bar" || "radar" || "area" || "heatmap")) {
+      setOptions(lineOptions), setSeries(lineSeries);
+    } else if (
+      type ===
+      ("donut" ||
+        "histogram" ||
+        "pie" ||
+        "radialBar" ||
+        "scatter" ||
+        "bubble" ||
+        "candleStick")
+    ) {
+      setOptions(pieOptions), setSeries(pieSeries);
+    }
   }, [type]);
 
   return (
@@ -57,8 +79,8 @@ export const App = () => {
         options={options}
         series={series}
         type={type} //"bar","line", "donut","area", "radar", "histogram", "pie", "radialBar", scatter, bubble, heatmap, candlestick
-        width={800}
-        height={520}
+        width={500}
+        height={350}
       />
     </div>
   );
